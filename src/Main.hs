8444b60431d64991e48b3fc0a4f5276426e876a6
Will Lane <williamlane923@gmail.com>
2021-02-19 10:53:22 -0800

    added loop syntax including a loop stack seperate from the other stack

--- src/Main.hs
+++ src/Main.hs
@@ -7,21 +7,28 @@ import Text.Parsec
 import Text.Parsec.String
 import Control.Monad.State
 
+debug :: Bool
+debug = False
+
 data Gardens = Print
   | Add
   | Sub
   | NL
   | Loop [Gardens]
+  | AddLoop
+  | SubLoop
   deriving (Show)
 
 generateParser :: Char -> Gardens -> Parser Gardens
 generateParser x y = char x >> return y
 
-parsePrint, parseAdd, parseSub, parseNL, parseLoop :: Parser Gardens
+parsePrint, parseAdd, parseSub, parseNL, parseLoop, parseAddLoop, parseSubLoop :: Parser Gardens
 parsePrint = generateParser '|' Print
 parseAdd = generateParser '{' Add
 parseSub = generateParser '}' Sub
 parseNL = generateParser '>' NL
+parseAddLoop = generateParser '[' AddLoop
+parseSubLoop = generateParser ']' SubLoop
 
 parseLoop = do
   _ <- char '@'
@@ -31,35 +38,40 @@ parseLoop = do
 
 parseNotchar :: Parser ()
 parseNotchar = do
-  _ <- many $ noneOf "|{}>@#"
+  _ <- many $ noneOf "|{}>@#[]"
   return ()
 
 gardenInstruction :: Parser Gardens
 gardenInstruction = do
   parseNotchar
-  i <- parseAdd <|> parseSub <|> parsePrint <|> parseNL <|> parseLoop
+  i <- parseAdd <|> parseSub <|>
+   parsePrint <|> parseNL <|>
+   parseLoop <|> parseAddLoop <|>
+   parseSubLoop
   parseNotchar
   return i
 
 parseGarden :: Parser [Gardens]
 parseGarden = many gardenInstruction
 
-type GardenRunner = StateT Word8 IO ()
+type GardenRunner = StateT (Word8, Int) IO ()
 runGarden :: Gardens -> GardenRunner
 runGardens :: [Gardens] -> GardenRunner
 runGardens = mapM_ runGarden
 
 runGarden Print = do
-  stack <- get
+  (stack, _) <- get
   liftIO $ putChar $ toEnum $ fromIntegral stack
 --  liftIO $ print $ stack
 
-runGarden Add = modify (\stack -> toEnum $ fromIntegral $ stack + 1)
-runGarden Sub = modify (\stack -> stack - 1)
+runGarden Add = modify (\(stack, loopStack) -> (toEnum $ fromIntegral $ stack + 1, loopStack))
+runGarden Sub = modify (\(stack, loopStack) -> (stack - 1, loopStack))
+runGarden AddLoop = modify (\(stack, loopStack) -> (stack, loopStack + 1))
+runGarden SubLoop = modify (\(stack, loopStack) -> (stack, loopStack - 1))
 
 runGarden loop@(Loop out) = do
-  stack <- get
-  case stack of
+  (_, loopStack) <- get
+  case loopStack of
     0 -> return ()
     _ -> runGardens out >> runGarden loop
 
@@ -74,5 +86,5 @@ main = do
   contents <- hGetContents file
   case parse parseGarden fileName contents of
     Left e -> print e
-    Right out -> evalStateT (runGardens out) 0
+    Right out -> evalStateT (runGardens out) (0, 0)
 --    Right out -> print out
